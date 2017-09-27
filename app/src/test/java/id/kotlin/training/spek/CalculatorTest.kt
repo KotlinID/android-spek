121348d91b5b11ec2d911b28655a4577701999f0
Budi Oktaviyan Suryanto <budioktaviyans@gmail.com>
2017-09-28 03:16:46 +0530

    [Budi] Adding more sample test

--- app/src/test/java/id/kotlin/training/spek/CalculatorTest.kt
+++ app/src/test/java/id/kotlin/training/spek/CalculatorTest.kt
@@ -21,5 +21,29 @@ class CalculatorTest : Spek({
                 assertEquals(8, sum)
             }
         }
+
+        on("Subtraction") {
+            val subtract = calculator.subtract(10, 3)
+
+            it("Should return the subtraction between first and second number") {
+                assertEquals(7, subtract)
+            }
+        }
+
+        on("Multiplying") {
+            val multiple = calculator.multiple(3, 3)
+
+            it("Should return the multiplying between first and second number") {
+                assertEquals(9, multiple)
+            }
+        }
+
+        on("Dividing") {
+            val divide = calculator.divide(20, 2)
+
+            it("Should return the dividing between first and second number") {
+                assertEquals(10, divide)
+            }
+        }
     }
 })
