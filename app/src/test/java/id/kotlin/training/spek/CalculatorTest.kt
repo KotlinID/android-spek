02a05d49cbb06e5588a96f6f48cc1f6731f547de
Budi Oktaviyan Suryanto <budi.suryanto@go-jek.com>
2018-01-27 08:36:55 +0700

    Add support for IDE

--- app/src/test/java/id/kotlin/training/spek/CalculatorTest.kt
+++ app/src/test/java/id/kotlin/training/spek/CalculatorTest.kt
@@ -5,10 +5,7 @@ import org.jetbrains.spek.api.dsl.given
 import org.jetbrains.spek.api.dsl.it
 import org.jetbrains.spek.api.dsl.on
 import org.junit.Assert.assertEquals
-import org.junit.platform.runner.JUnitPlatform
-import org.junit.runner.RunWith
 
-@RunWith(JUnitPlatform::class)
 class CalculatorTest : Spek({
 
     given("Some calculator") {
