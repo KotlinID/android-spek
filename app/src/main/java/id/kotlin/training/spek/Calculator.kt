1f8093f14216ed8ae981b57f8bc0fef81b15ea6c
Budi Oktaviyan Suryanto <budi.suryanto@go-jek.com>
2018-01-27 08:20:22 +0700

    Update libs

--- app/src/main/java/id/kotlin/training/spek/Calculator.kt
+++ app/src/main/java/id/kotlin/training/spek/Calculator.kt
@@ -3,10 +3,7 @@ package id.kotlin.training.spek
 class Calculator {
 
     fun sum(x: Int, y: Int): Int = x.plus(y)
-
     fun subtract(x: Int, y: Int) = x.minus(y)
-
     fun multiple(x: Int, y: Int) = x.times(y)
-
     fun divide(x: Int, y: Int) = x.div(y)
 }
